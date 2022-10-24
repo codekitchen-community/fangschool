3c8ed34513f6757cb5fffde26ab2f8d3794d702a
Grey Li <withlihui@gmail.com>
2022-10-24 22:34:07 +0800

    Remove test data

--- app.py
+++ app.py
@@ -9,26 +9,12 @@ bootstrap = Bootstrap5(app)
 def index():
     return render_template('index.html')
 
+
 @app.route('/construction_site')
 def construction_site():
     return render_template('construction_site.html')
 
 
-from random import randrange, choice
-
-
 @app.route('/playground')
 def playground():
-    students = []
-    names = ['Grey', 'Good', 'Peter', 'cook24', 'fi22', 'molly', 'jackss', '彩蛋', '旺福', '赵杰', '李辉']
-    for i in range(50):
-        rand_color = (randrange(255), randrange(255), randrange(255))
-
-        students.append({
-            'width': randrange(50, 200),
-            'height': randrange(50, 200),
-            'name': choice(names),
-            'color': f'rgb{rand_color}',
-        })
-    print(students)
-    return render_template('playground.html', students=students)
+    return render_template('playground.html')
